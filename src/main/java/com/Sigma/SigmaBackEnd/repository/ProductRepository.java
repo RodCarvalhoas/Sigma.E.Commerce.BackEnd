216988edb9e1f00063adf6b9513aebb27beb50f8
RodCarvalhoas <sutil_rodrigo@hotmail.com>
2023-11-03 13:56:20 -0300

    feat/Query findAllByCategory in ProductRepository created

--- src/main/java/com/Sigma/SigmaBackEnd/repository/ProductRepository.java
+++ src/main/java/com/Sigma/SigmaBackEnd/repository/ProductRepository.java
@@ -2,10 +2,17 @@ package com.Sigma.SigmaBackEnd.repository;
 
 import com.Sigma.SigmaBackEnd.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    @Query("SELECT obj FROM Product obj WHERE obj.category.id = :id_cat ORDER BY obj.name")
+    List<Product> findAllByCategory(@Param(value = "id_cat") UUID id_cat);
+
 }
--- src/main/java/com/Sigma/SigmaBackEnd/services/ProductService.java
+++ src/main/java/com/Sigma/SigmaBackEnd/services/ProductService.java
@@ -1,8 +1,14 @@
 package com.Sigma.SigmaBackEnd.services;
 
+import com.Sigma.SigmaBackEnd.excepitons.ObjectNotFoundException;
+import com.Sigma.SigmaBackEnd.model.Product;
 import com.Sigma.SigmaBackEnd.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -10,5 +16,17 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public Product findById(UUID id){
+        Optional<Product> product = productRepository.findById(id);
+        return product.orElseThrow(() -> new ObjectNotFoundException("Produto com id: " + id + " não encontrado"));
+    }
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public List<Product> findAllByCategory(UUID CategoryId){
+        return productRepository.findAllByCategory(CategoryId);
+    }
 
 }
